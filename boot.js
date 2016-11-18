0aca10de8a94a792ddb9197d6b3940c0909876bf
Tomasz Libich <tomasz.libich@ft.com>
2016-11-18 09:57:47 +0000

    Testing fix at Heroku

--- boot.js
+++ boot.js
@@ -8,14 +8,14 @@
     const express = require('express'),
         //handlerFor404 = require('./config/404'),
         middlewares = require('./config/middlewares'),
-        Api = require('./api'),
+        //Api = require('./api'),
         //Poller = require('./poller'),
         app = express();
 
     middlewares.configure(app);
 
-    app.all('/api/:command', Api.handle);
-    app.all('/api/:command/:key', Api.handle);
+    //app.all('/api/:command', Api.handle);
+    //app.all('/api/:command/:key', Api.handle);
     //app.get('/__health', Api.healthcheck);
 
     //Poller.init();
