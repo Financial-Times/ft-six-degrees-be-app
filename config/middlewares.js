f0cdd2f6ca4768847afcfc7381774ece6398c6df
Tomasz Libich <tomasz.libich@ft.com>
2016-11-18 09:25:56 +0000

    Testing port

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const bodyParser = require('body-parser'),
-    CONFIG = require('./'),
-    cors = require('./cors'),
-    //authS3O = require('s3o-middleware'),
-    winston = require('../winston-logger');
+    cors = require('./cors');
+    //authS3O = require('s3o-middleware');
 
 function configure(app) {
     //app.use(authS3O);
@@ -12,11 +10,6 @@ function configure(app) {
     app.use(bodyParser.urlencoded({
         extended: false
     }));
-    console.log('PORT', CONFIG.PORT, process.env.PORT);
-    app.listen(CONFIG.PORT, function () {
-        winston.logger.info('[boot] Running server on port ' + CONFIG.PORT + '...');
-    });
-
 }
 
 module.exports = {
