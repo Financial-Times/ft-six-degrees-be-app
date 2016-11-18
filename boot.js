08de80c5570cc124ed8f8116cbcd7370ea8da5cf
Tomasz Libich <tomasz.libich@ft.com>
2016-11-18 10:20:38 +0000

    Testing fix at Heroku

--- boot.js
+++ boot.js
@@ -7,7 +7,7 @@
 
     const express = require('express'),
         CONFIG = require('./config'),
-        //handlerFor404 = require('./config/404'),
+        handlerFor404 = require('./config/404'),
         winston = require('./winston-logger'),
         app = express();
 
@@ -17,16 +17,15 @@
 
     require('./config/middlewares').configure(app);
 
-    const Api = require('./api'),//eslint-disable-line one-var
+    const Api = require('./api'), //eslint-disable-line one-var
         Poller = require('./poller');
 
+    app.all('/api/:command', Api.handle);
+    app.all('/api/:command/:key', Api.handle);
+    app.get('/__health', Api.healthcheck);
 
-    //app.all('/api/:command', Api.handle);
-    //app.all('/api/:command/:key', Api.handle);
-    //app.get('/__health', Api.healthcheck);
+    Poller.init();
 
-    //Poller.init();
-
-    //app.use(handlerFor404);
+    app.use(handlerFor404);
 
 }());
