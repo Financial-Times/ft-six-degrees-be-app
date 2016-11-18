e83f3915765bf56cf8ecb4ed84b3d3383c306e80
Tomasz Libich <tomasz.libich@ft.com>
2016-11-18 09:40:28 +0000

    Fixing issue at Heroku env

--- boot.js
+++ boot.js
@@ -6,18 +6,11 @@
     }
 
     const express = require('express'),
-        winston = require('./winston-logger'),
-        CONFIG = require('./config'),
         handlerFor404 = require('./config/404'),
         middlewares = require('./config/middlewares'),
         Api = require('./api'),
         Poller = require('./poller'),
         app = express();
-
-    console.log('PORT', CONFIG.PORT, process.env.PORT);
-    app.listen(CONFIG.PORT, function () {
-        winston.logger.info('[boot] Running server on port ' + CONFIG.PORT + '...');
-    });
 
     middlewares.configure(app);
 
