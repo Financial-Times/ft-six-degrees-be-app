c9aadbe05bf7cbe99161e5968a78e3b9953ce2c1
Tomasz Libich <tomasz.libich@ft.com>
2016-11-22 09:41:36 +0000

    Updating getHistory method in personalised-people module

--- api/personalised-people/index.js
+++ api/personalised-people/index.js
@@ -7,14 +7,37 @@ const request = require('request'),
     PersonalisedPeopleStorage = require('../../cache/personalised-people-storage'),
     winston = require('../../winston-logger');
 
+function getRecency(key) {
+    let recency;
+    console.warn('XXXXk', key);
+    switch (key) {
+    case 'year':
+        recency = '365';
+        break;
+    case 'month':
+        recency = '30';
+        break;
+    case 'week':
+        recency = '7';
+        break;
+    case 'day':
+        recency = '1';
+        break;
+    default:
+        recency = '30';
+        break;
+    }
+    return recency;
+}
+
 function getHistory(key, uuid, res) {
-    request(CONFIG.URL.API.FT_RECOMMENDATIONS_USERS + uuid + '/history?limit=50&recency=7&apiKey=' + CONFIG.API_KEY.FT_RECOMMENDATIONS, function (error, resp, history) {
+    request(CONFIG.URL.API.FT_RECOMMENDATIONS_USERS + uuid + '/history?limit=100&recency=' + getRecency(key) + '&apiKey=' + CONFIG.API_KEY.FT_RECOMMENDATIONS, function (error, resp, history) {
         if (resp && resp.statusCode === 200) {
             EnrichedContent.getPeople(res, JSON.parse(history).response, key);
         } else if (!error && resp) {
             responder.rejectNotFound(res);
         } else {
-            winston.logger.error('[api-personalised-people] ' + error + ' URL ' + CONFIG.URL.API.FT_RECOMMENDATIONS_USERS + uuid + '/history?limit=50&recency=7&apiKey=');
+            winston.logger.error('[api-personalised-people] ' + error);
             responder.rejectBadGateway();
         }
     });
