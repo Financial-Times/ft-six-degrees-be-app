916d329e9c42f884337b111c7aac5175e56e7553
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-05-11 11:09:20 +0300

    (WIP) Add image urls to connections

--- api/connections/index.js
+++ api/connections/index.js
@@ -21,7 +21,7 @@ function fetch(key, uuid) {
         toDate = datesRange[1];
 
     return new Promise(function (resolve, reject) {
-        request(CONFIG.URL.API.SIX_DEGREES_HOST + 'connectedPeople?minimumConnections=2&fromDate=' + fromDate + '&toDate=' + toDate + '&limit=50&contentLimit=20&uuid=' + uuid, {
+        request(CONFIG.URL.API.SIX_DEGREES_HOST + 'connectedPeople?minimumConnections=2&fromDate=' + fromDate + '&toDate=' + toDate + '&limit=10&contentLimit=20&uuid=' + uuid, {
             headers: {
                 'Authorization': 'Basic ' + CONFIG.API_KEY.SIX_DEGREES
             }
@@ -33,6 +33,45 @@ function fetch(key, uuid) {
             }
         });
     });
+}
+
+function getImage(person) {
+	return new Promise(function (resolve, reject) {
+		request('https://en.wikipedia.org/w/api.php?action=query&titles=' + person.abbrName + '&prop=pageimages&format=json&pithumbsize=600', function (err, response, body) {
+
+			if (err) {
+				reject(err);
+			} else {
+
+				let key, url;
+
+				body = jsonHandler.parse(body);
+
+				if (body.query && body.query.pages) {
+					for (key in body.query.pages) {
+						if (body.query.pages.hasOwnProperty(key)) {
+							url = body.query.pages[key].thumbnail ? body.query.pages[key].thumbnail.source : null;
+						}
+					}
+				}
+
+				resolve({
+					url: url
+				});
+			}
+		});
+	});
+}
+
+function getNameInitials(prefLabel) {
+	const initials = prefLabel.match(/\b\w/g) || [];
+	return ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+}
+
+function getAbbreviatedName(prefLabel) {
+	const prefLabelArray = prefLabel.split(' '),
+	      max = prefLabelArray.length;
+	return prefLabelArray[0] + ' ' + prefLabelArray[max - 1];
 }
 
 class Connections {
@@ -48,8 +87,30 @@ class Connections {
             respond(res, jsonHandler.parse(cached));
         } else {
             fetch(key, uuid).then(body => {
-                connectionsStorage.cache(today, uuid, key, body);
-                respond(res, jsonHandler.parse(connectionsStorage.get(today, uuid, key)));
+				let connections = jsonHandler.parse(body)
+					.map(conn => {
+						conn.person = Object.assign({}, conn.person, {
+							abbrName: getAbbreviatedName(conn.person.prefLabel),
+							initials: getNameInitials(conn.person.prefLabel)
+						});
+						return conn;
+					});
+	            const actions = connections && connections.length ? connections.map(conn => getImage(conn.person)) : [],
+	                  results = actions.length ? Promise.all(actions) : null;
+
+	            if (results) {
+		            results.then(images => {
+			            images.map((img, index) => {
+				            if (img && img.url) {
+					            connections[index].person.img = img.url;
+				            }
+			            });
+			            connectionsStorage.cache(today, uuid, key, JSON.stringify(connections));
+			            respond(res, connections);
+		            }).catch(error => {
+			            winston.logger.error('[parsers-common-people-images]\n\n' + error);
+		            });
+	            }
             }).catch(error => {
                 respond(res, error);
             });
