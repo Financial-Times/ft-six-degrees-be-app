671cd99d54075c2bfc78f3394d39e8c777a35dcf
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-09-11 10:58:06 +0300

    Do not cache article authors

--- parsers/enriched-content/index.js
+++ parsers/enriched-content/index.js
@@ -2,6 +2,7 @@
 
 const request = require('request'),
     winston = require('../../winston-logger'),
+    uniqBy = require('lodash/uniqBy'),
     CONFIG = require('../../config'),
     personalisedPeopleStorage = require('../../cache/personalised-people-storage');
 
@@ -21,39 +22,34 @@ function getEnrichedContent(article) {
     });
 }
 
+const getAnnotationPredicate = predicate => {
+    return predicate.substring(predicate.lastIndexOf('/') + 1, predicate.length);
+};
+
 class EnrichedContent {
 
     getPeople(clientRes, history, key) {
-        const articles = history.articles,
+        const articles = uniqBy(history.articles, 'id'),
             actions = articles && articles.length ? articles.map(getEnrichedContent) : [],
             results = actions.length ? Promise.all(actions) : null;
 
         if (results) {
             results.then(enrichedcontent => {
-                const annotatedPeople = [];
+                let annotatedPeople = [];
 
                 enrichedcontent.map(content => {
                     content.annotations.map(annotation => {
-                        if (annotation.type === 'PERSON') {
-                            let alreadyInAnnotatedArray = false;
-
-                            annotatedPeople.map(annotatedPerson => {
-                                if (annotatedPerson.id === annotation.id) {
-                                    alreadyInAnnotatedArray = true;
-                                }
-                            });
-
-                            if (!alreadyInAnnotatedArray) {
-                                annotatedPeople.push({
-                                    id: annotation.id,
-                                    prefLabel: annotation.prefLabel
-                                });
-                            }
-                        }
+                        if (annotation.type === 'PERSON' && getAnnotationPredicate(annotation.predicate) !== 'hasAuthor') {
+							annotatedPeople.push({
+								id: annotation.id,
+								prefLabel: annotation.prefLabel
+							});
+						}
                     });
                 });
+                const uniqueAnnotatedPeople = uniqBy(annotatedPeople, 'id');
 
-                personalisedPeopleStorage.cache(JSON.stringify(annotatedPeople), key, clientRes);
+                personalisedPeopleStorage.cache(JSON.stringify(uniqueAnnotatedPeople), key, clientRes);
 
             }).catch(error => {
                 winston.logger.error('[parsers-mentioned-people-articles]\n\n' + error);
