13155108b4506af5c19d1d5cb9ee09e1cb98ef5f
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-09-11 10:55:57 +0300

    Add date range to annotated articles request

--- parsers/common/annotated-articles.js
+++ parsers/common/annotated-articles.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const request = require('request'),
+      uniqBy = require('lodash/uniqBy'),
+      datesHandler = require('../../utils/dates-handler'),
     winston = require('../../winston-logger');
 
-function getArticles(person) {
+function getArticles(person, key) {
+    const dates = key ? datesHandler.getRange(key) : ['', ''];
     return new Promise(function (resolve, reject) {
-        request(process.env.FT_API_URL + 'content?isAnnotatedBy=' + person.id, {
+        request(process.env.FT_API_URL + 'content?isAnnotatedBy=' + person.id + '&fromDate=' + dates[0] + '&toDate=' + dates[1], {
             headers: {
                 'x-api-key': process.env.FT_API_KEY
             }
@@ -13,9 +16,10 @@ function getArticles(person) {
             if (error) {
                 reject(error);
             } else {
+                const uniqueContent = uniqBy(JSON.parse(content), 'id').filter(item => item.hasOwnProperty('id'));
                 resolve({
                     id: person.id,
-                    content: JSON.parse(content)
+                    content: uniqueContent
                 });
             }
         });
@@ -28,9 +32,9 @@ class AnnotatedArticles {
         this.articlesStorage = articlesStorage;
     }
 
-    handle(people, articlesCallback) {
+    handle(people, articlesCallback, key) {
 
-        const actions = people && people.length ? people.map(getArticles) : [],
+        const actions = people && people.length ? people.map(person => getArticles(person, key)) : [],
             results = actions.length ? Promise.all(actions) : null;
 
         if (results) {
