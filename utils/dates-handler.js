7d3be9261616d30d60e5212c7555e9067fdc09d9
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-09-11 10:57:01 +0300

    Refactor dates handler

--- utils/dates-handler.js
+++ utils/dates-handler.js
@@ -8,16 +8,10 @@ class DatesHandler {
         format = format || 'YYYY-MM-DD';
         let dateParams = key.split(' ');
 
-        let fromDate = null;
-        const today = moment().startOf('day').format(format);
-        if (dateParams.length < 2 || isNaN(parseInt(dateParams[0], 10))) {
-            fromDate = moment().subtract(1, 'month');
-        } else {
-	        fromDate = moment()
-		        .subtract(parseInt(dateParams[0] || 1, 10), dateParams[1] || 'months');
-        }
+        const today = moment().startOf('day');
+        const fromDate = moment(today).subtract(parseInt((dateParams[1] ? dateParams[0] : 1), 10), dateParams[1] || 'months');
 
-        return [fromDate.format(format), today];
+        return [fromDate.format(format), today.format(format)];
     }
 }
 
