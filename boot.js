a5febe642b4094ce3873643c13cb6e7194fba054
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-10-16 09:53:39 +0300

    Update boot script

--- boot.js
+++ boot.js
@@ -1,19 +1,15 @@
 'use strict';
 
-if (require('fs-extra').existsSync('.env.json')) {//eslint-disable-line no-sync
-    require('dot-env');
-}
-
 const app = require('express')(),
-    CONFIG = require('./config'),
-    handlerFor404 = require('./middlewares/404'),
-    middlewares = require('./middlewares'),
-    Api = require('./api'),
-    Poller = require('./poller'),
-    winston = require('./winston-logger');
+	CONFIG = require('./config'),
+	handlerFor404 = require('./middlewares/404'),
+	middlewares = require('./middlewares'),
+	Api = require('./api'),
+	Poller = require('./poller'),
+	winston = require('./winston-logger');
 
-app.listen(CONFIG.PORT, function () {
-    winston.logger.info('[boot] Running server on port ' + CONFIG.PORT + '...');
+app.listen(CONFIG.PORT, () => {
+	winston.logger.info(`[boot] Running server on port ${CONFIG.PORT}...`);
 });
 
 middlewares.configure(app);
@@ -26,5 +22,3 @@ app.get('/__health', Api.healthcheck);
 Poller.init();
 
 app.use(handlerFor404);
-
-
